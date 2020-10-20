13d5c3786ac78b9b269eb9942a5e53153bb5e0b3
Tydroneous <69044026+Tydroneous@users.noreply.github.com>
2020-10-20 12:47:41 -0400

    Initial commit

---
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -0,0 +1,9 @@
+# Write your code here
+choice = input()
+
+if choice == "scissors":
+    print("Sorry, but the computer chose rock")
+elif choice == "paper":
+    print("Sorry, but the computer chose scissors")
+else:
+    print("Sorry, but the computer chose paper")
